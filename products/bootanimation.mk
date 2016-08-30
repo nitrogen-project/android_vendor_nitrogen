a687e9d78193e776fa1095223fec2dbf6fadf9e1
root <xyyx@mail.ru>
2016-08-30 14:48:55 +0800

    Init

---
+++ products/bootanimation.mk
@@ -0,0 +1,46 @@
+# Copyright (C) 2016 Nitrogen Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifneq ($(filter nitrogen_geeb,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nitrogen/prebuilt/common/bootanimation/768x1280.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter nitrogen_geehrc,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nitrogen/prebuilt/common/bootanimation/768x1280.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter nitrogen_mako,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nitrogen/prebuilt/common/bootanimation/768x1280.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter nitrogen_hammerhead,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nitrogen/prebuilt/common/bootanimation/1080x1920.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter nitrogen_bullhead,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nitrogen/prebuilt/common/bootanimation/1080x1920.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter nitrogen_shamu,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nitrogen/prebuilt/common/bootanimation/1440x2560.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter nitrogen_sprout4,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nitrogen/prebuilt/common/bootanimation/480x854.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter nitrogen_sprout8,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nitrogen/prebuilt/common/bootanimation/480x854.zip:system/media/bootanimation.zip
+endif
---
+++ products/common.mk
@@ -0,0 +1,55 @@
+# Generic product
+PRODUCT_NAME := nitrogen
+PRODUCT_BRAND := nitrogen
+PRODUCT_DEVICE := generic
+
+PRODUCT_BUILD_PROP_OVERRIDES := BUILD_DISPLAY_ID=$(TARGET_PRODUCT)-$(PLATFORM_VERSION)-$(BUILD_ID)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    ro.com.android.dataroaming=false \
+    drm.service.enabled=true \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    ro.build.selinux=1 \
+    ro.adb.secure=0
+
+# Common overlay
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/nitrogen/overlay/common
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Latin IME lib - gesture typing
+PRODUCT_COPY_FILES += \
+    vendor/nitrogen/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    Busybox \
+    LockClock \
+    Stk \
+    Terminal
+
+# Init.d script support
+PRODUCT_COPY_FILES += \
+    vendor/nitrogen/prebuilt/common/bin/sysinit:system/bin/sysinit \
+    vendor/nitrogen/prebuilt/common/etc/init.d.rc:root/init.d.rc
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/nitrogen/prebuilt/common/addon.d/50-nitrogen.sh:system/addon.d/50-nitrogen.sh \
+    vendor/nitrogen/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/nitrogen/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions
+
+# Boot animations
+$(call inherit-product-if-exists, vendor/nitrogen/products/bootanimation.mk) 
