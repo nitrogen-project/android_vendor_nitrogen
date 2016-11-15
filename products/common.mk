9f23de2110a8bd936e2e7ad4791d3b372b2fac53
snak3ater <saeedyasir91@gmail.com>
2016-11-15 21:19:20 +0800

    Enable Storage manager [1/2]

--- products/common.mk
+++ products/common.mk
@@ -21,7 +21,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true \
     persist.sys.dun.override=0 \
     ro.build.selinux=1 \
-    ro.adb.secure=0
+    ro.adb.secure=0 \
+    ro.storage_manager.enabled=1
 
 # Needs for MTP Dirty Hack
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
