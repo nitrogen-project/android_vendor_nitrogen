723d814b1e65e2e46f432f31bba09bafdd073119
zvnexus <zvnexus@gmail.com>
2018-09-23 03:58:00 -0400

    New default launcher and music player

--- products/common.mk
+++ products/common.mk
@@ -87,3 +87,6 @@ PRODUCT_COPY_FILES += \
 
 # Boot animations
 $(call inherit-product-if-exists, vendor/nitrogen/products/bootanimation.mk)
+
+# Additional packages
+-include vendor/nitrogen/products/packages.mk
