3825254dd1bb4b8cf0b30d3f26564d2af626acb6
Pig <pig.priv@gmail.com>
2020-10-15 02:58:40 +0800

    soong: Add aosp soong config variables
    Ref: https://android.googlesource.com/platform/build/soong/+/refs/tags/android-11.0.0_r4#soong-config-variables
    
    Change-Id: Icaf35b65b63d4245c92e39ce628bb8d6ffd0d4bb

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -1,28 +1,28 @@
 package generator
 
 import (
-	"fmt"
+    "fmt"
 
-	"android/soong/android"
+    "android/soong/android"
 )
 
-func nitrogenExpandVariables(ctx android.ModuleContext, in string) string {
-	nitrogenVars := ctx.Config().VendorConfig("nitrogenVarsPlugin")
+func aospExpandVariables(ctx android.ModuleContext, in string) string {
+    aospVars := ctx.Config().VendorConfig("aospVarsPlugin")
 
-	out, err := android.Expand(in, func(name string) (string, error) {
-		if nitrogenVars.IsSet(name) {
-			return nitrogenVars.String(name), nil
-		}
-		// This variable is not for us, restore what the original
-		// variable string will have looked like for an Expand
-		// that comes later.
-		return fmt.Sprintf("$(%s)", name), nil
-	})
-
-	if err != nil {
-		ctx.PropertyErrorf("%s: %s", in, err.Error())
-		return ""
+    out, err := android.Expand(in, func(name string) (string, error) {
+	if aospVars.IsSet(name) {
+	    return aospVars.String(name), nil
 	}
+	// This variable is not for us, restore what the original
+	// variable string will have looked like for an Expand
+	// that comes later.
+	return fmt.Sprintf("$(%s)", name), nil
+    })
 
-	return out
+    if err != nil {
+	ctx.PropertyErrorf("%s: %s", in, err.Error())
+	return ""
+    }
+
+    return out
 }
