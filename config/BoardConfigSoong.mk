4e610f36cd1a0b71b94f344b0c0d4216f5f6545f
daniml3 <danimoral1001@gmail.com>
2021-01-21 16:56:41 +0100

    soong: introduce TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY
    Set TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY to the key code to skip in InputDispatcher

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -24,8 +24,19 @@ define addVar
   SOONG_CONFIG_nitrogenVarsPlugin_$(1) := $$(subst ",\",$$($1))
 endef
 
+
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+
+SOONG_CONFIG_NAMESPACES += nitrogenGlobalVars
+SOONG_CONFIG_nitrogenGlobalVars += \
+    target_inputdispatcher_skip_event_key
 
 ifneq ($(TARGET_USE_QTI_BT_STACK),true)
 PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
 endif #TARGET_USE_QTI_BT_STACK
+
+# Set default values
+TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY ?= 0
+
+# Soong value variables
+SOONG_CONFIG_nitrogenGlobalVars_target_inputdispatcher_skip_event_key := $(TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY)
