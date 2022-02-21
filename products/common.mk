a492b02e5d92a4d1cc27dfd300d099295b6827c4
jhenrique09 <jhenrique09.mcz@hotmail.com>
2022-02-21 20:42:02 +0300

    Restore FaceUnlock

--- products/common.mk
+++ products/common.mk
@@ -86,14 +86,15 @@ PRODUCT_PACKAGES += \
     NavigationBarMode2ButtonOverlay
 
 # Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := false
-ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
 PRODUCT_PACKAGES += \
     FaceUnlockService
-TARGET_FACE_UNLOCK_SUPPORTED := true
-endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
 
 # Init.d script support
 PRODUCT_COPY_FILES += \
